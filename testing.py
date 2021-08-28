24dd06cb1bb43ab100e38b8be762de3ebdc871fd
Atul Sutar <78650734+Atul-Sutar@users.noreply.github.com>
2021-08-28 20:36:50 +0530

    final works on both linux and windows

--- testing.py
+++ testing.py
@@ -11,11 +11,11 @@ def exec(cmd):
     cmd = cmd.strip()
     print(shlex.split(cmd))
     cmd_dict = shlex.split(cmd)
-    if cmd_dict[0] == "cd" and os.name == "nt":
-        if len(cmd_dict) == 1:
+    if os.name == "nt" and cmd_dict[0] == "cd":
+        if  len(cmd_dict) == 1:
             get_cur_dir = os.getcwd()
             return get_cur_dir.encode()
-        elif cmd_dict[0] and cmd_dict[1] == "..":
+        elif cmd_dict[1] == "..":
             current = os.getcwd()
             cur_splitted = current.split("\\")[:-1]
             #dir_splitted = cur_splitted[:-1]
@@ -27,7 +27,8 @@ def exec(cmd):
             os.chdir(cmd_dict[1])
             #out_dir = f"Changed to {os.getcwd()}"
             return f"{os.getcwd()} ".encode()
-    elif cmd_dict[0] == "cd" and os.name == "posix":
+
+    if cmd_dict[0] == "cd" and os.name == "posix":
         if len(cmd_dict) == 1:
             os.chdir("~/")
             return get_cur_dir.encode()
@@ -41,16 +42,55 @@ def exec(cmd):
             os.chdir(cmd_dict[1])
             return f"{os.getcwd()} ".encode()
 
-    elif cmd:
-        output = subprocess.Popen(shlex.split(cmd),stderr=subprocess.STDOUT,stdout=subprocess.PIPE,shell=True)
-        out =  output.stdout.read()
-        #print(out)
-        if out == b"":
+    if cmd_dict and os.name == "nt":
+        output = subprocess.Popen(cmd_dict,stderr=subprocess.STDOUT,stdout=subprocess.PIPE,shell=True)
+        if output.stdout.read() == b"":
             return b"No Output\n\r"
         else:
-            return out
-        # return str(out)+str(err)
+            return output
+            # return str(out)+str(err)
+        
+    elif cmd_dict and os.name == 'posix':
+        output = subprocess.Popen(shlex.split(cmd),stderr=subprocess.STDOUT,stdout=subprocess.PIPE)
+        return output.stdout.read()
     return "Command Not Entered"
+    
+    
+
+# def not_in_use_base():
+#     ip = "127.0.0.1"
+#     port = 43
+
+#     def main():
+#         sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#         sock.bind((ip,port))
+#         sock.listen(5)
+#         print(f"listening on {ip}:{port}")
+#         try:
+#             while True:
+#                 client, addr = sock.accept()
+#                 print(addr,"connected")
+#                 handle = Thread(target=client_handle,args=(client,addr))
+#                 handle.start()
+#         except:
+#             sock.close()
+#             print("terminated")
+
+#     def client_handle(client_socket,address):
+#         addr = address
+#         try:
+#             with client_socket as sock:
+#                 #while True: 
+#                 recv = sock.recv(10240) 
+#                     #print(recv.decode()) 
+#                 try:
+#                     sock.send(exec(recv.decode()))
+#                 except :
+#                     sock.send(b"Error Occured")
+#         except:
+#             client_socket.close()
+#             print("connection closed from",addr)
+
 
 def main(args):
     if args.listen:
@@ -73,7 +113,7 @@ def main(args):
             sock.connect((args.target,args.port))
             file_buffer = b''
             while True:
-                data = sock.recv(4096)
+                data = sock.recv(10485760)
                 if data:
                     file_buffer += data
                 else:
@@ -92,7 +132,7 @@ def main(args):
             sock.connect((args.target,args.port))
             data = args.execute
             sock.send(data.encode())
-            recv = sock.recv(4096)
+            recv = sock.recv(10485760)
             print(recv.decode())
         except:
             sock.close()
@@ -100,7 +140,7 @@ def main(args):
             sys.exit()
     elif args.shell:
         sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        sock.connect(("127.0.0.1",43))
+        sock.connect((args.target,args.port))
         try:
             while True:
                 data = input(r"{shell}:> ")
@@ -108,7 +148,7 @@ def main(args):
                     sock.send(b" ")
                 else:    
                     sock.send(data.encode())
-                recv = sock.recv(10240)
+                    recv = sock.recv(10485760)
                 print(recv.decode())
         except KeyboardInterrupt:
             sock.close()
@@ -123,10 +163,10 @@ def client_handle(client_socket,address):
         try:
             with client_socket as sock:
                 while True: 
-                    recv = sock.recv(4096) 
+                    recv = sock.recv(10485760) 
                     #print(recv.decode()) 
                     try:
-                        sock.send(exec(recv.decode()))
+                        sock.sendall(exec(recv.decode()))
                     except :
                         sock.send(b"\n\r")
         except:
@@ -140,7 +180,7 @@ def client_handle(client_socket,address):
                 recv = sock.recv(4096) 
                         #print(recv.decode()) 
                 try:
-                    sock.send(exec(recv.decode()))
+                    sock.sendall(exec(recv.decode()))
                 except :
                     sock.send(b"Error Occured")
         except:
@@ -149,25 +189,24 @@ def client_handle(client_socket,address):
         finally:
             client_socket.close()
             print("connection closed from",addr)
-            
 
-parser = argparse.ArgumentParser(description="Netcat Tool",formatter_class=
-argparse.RawDescriptionHelpFormatter,epilog=textwrap.dedent("""
-Example:
-netcat.py -t  192.168.1.108 -p 5555 -l -c # command shell
-netcat.py -t  192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
-netcat.py -t  192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
-netcat.py -t  192.168.1.108 -p 5555  #  connect to server
-echo 'ABC' |  ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135"""))
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Netcat Tool",formatter_class=
+    argparse.RawDescriptionHelpFormatter,epilog=textwrap.dedent("""
+    Example:
+    netcat.py -t  192.168.1.108 -p 5555 -l -c # command shell
+    netcat.py -t  192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
+    netcat.py -t  192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
+    netcat.py -t  192.168.1.108 -p 5555  #  connect to server
+    echo 'ABC' |  ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135"""))
+    
 parser.add_argument("-s","--shell",action='store_true',help='command shell')
 parser.add_argument('-e', '--execute', help='execute specified command')
 parser.add_argument('-l', '--listen',action='store_true',help='listen')
 parser.add_argument('-p', '--port',type=int, help='specified port')
 parser.add_argument('-t', '--target',help='specified IP')
 parser.add_argument('-u', '--upload', help='upload file')
-    
+
 args = parser.parse_args()
-  
-if __name__ == "__main__":
-    main(args)
+
+main(args)
