a982fe53aed2fd3ec80f201ab50dd8232a877f9a
Atul Sutar <78650734+Atul-Sutar@users.noreply.github.com>
2021-08-28 22:41:01 +0530

    resolved command issue
    some linux and windows conflict command execution

--- testing.py
+++ testing.py
@@ -1,4 +1,5 @@
 from threading import Thread
+from tqdm import tqdm
 import os
 import socket
 import argparse
@@ -44,53 +45,17 @@ def exec(cmd):
 
     if cmd_dict and os.name == "nt":
         output = subprocess.Popen(cmd_dict,stderr=subprocess.STDOUT,stdout=subprocess.PIPE,shell=True)
-        if output.stdout.read() == b"":
+        out =  output.stdout.read()
+        #print(out)
+        if out == b"":
             return b"No Output\n\r"
         else:
-            return output
-            # return str(out)+str(err)
+            return out
         
     elif cmd_dict and os.name == 'posix':
         output = subprocess.Popen(shlex.split(cmd),stderr=subprocess.STDOUT,stdout=subprocess.PIPE)
         return output.stdout.read()
     return "Command Not Entered"
-    
-    
-
-# def not_in_use_base():
-#     ip = "127.0.0.1"
-#     port = 43
-
-#     def main():
-#         sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#         sock.bind((ip,port))
-#         sock.listen(5)
-#         print(f"listening on {ip}:{port}")
-#         try:
-#             while True:
-#                 client, addr = sock.accept()
-#                 print(addr,"connected")
-#                 handle = Thread(target=client_handle,args=(client,addr))
-#                 handle.start()
-#         except:
-#             sock.close()
-#             print("terminated")
-
-#     def client_handle(client_socket,address):
-#         addr = address
-#         try:
-#             with client_socket as sock:
-#                 #while True: 
-#                 recv = sock.recv(10240) 
-#                     #print(recv.decode()) 
-#                 try:
-#                     sock.send(exec(recv.decode()))
-#                 except :
-#                     sock.send(b"Error Occured")
-#         except:
-#             client_socket.close()
-#             print("connection closed from",addr)
-
 
 def main(args):
     if args.listen:
@@ -108,31 +73,32 @@ def main(args):
             sock.close()
             print("terminated")
     elif args.upload:
-        try:
-            sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            sock.connect((args.target,args.port))
-            file_buffer = b''
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.connect((args.target,args.port))
+        f = args.file
+        fl = open(f,"rb")
+        fl.seek(0,os.SEEK_END)
+        filesize = fl.tell()
+        sock.send(f"{f}{'-'}{filesize}".encode())
+        fl.close()
+        progress = tqdm(range(filesize), f"Progress | ",unit="B", unit_scale=True, unit_divisor=1024)
+        with open(f,"rb") as fle:
             while True:
-                data = sock.recv(10485760)
-                if data:
-                    file_buffer += data
-                else:
+                bytes_read = fle.read(10485760)
+                if not bytes_read:
                     break
-            with open(args.upload, 'wb') as f:
-                f.write(file_buffer)
-            message = f'Saved file {args.upload}'
-            sock.send(message.encode())
-        except:
-            sock.close()
-            sys.exit()
-
+                sock.sendall(bytes_read)
+                progress.update(len(bytes_read))
+        print(f"file uploaded succesfully {f}")
+        sock.close()
+        sys.exit()
     elif args.execute:
         try:
             sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
             sock.connect((args.target,args.port))
             data = args.execute
             sock.send(data.encode())
-            recv = sock.recv(10485760)
+            recv = sock.recv(4096)
             print(recv.decode())
         except:
             sock.close()
@@ -144,17 +110,35 @@ def main(args):
         try:
             while True:
                 data = input(r"{shell}:> ")
-                if data == "":
-                    sock.send(b" ")
-                else:    
-                    sock.send(data.encode())
-                    recv = sock.recv(10485760)
+                sock.send(data.encode())
+                recv = sock.recv(4096)
                 print(recv.decode())
         except KeyboardInterrupt:
             sock.close()
             print("Connection closed successfully")
             sys.exit()
 
+    elif args.download:
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.bind((args.target,args.port))
+        sock.listen(2)
+        client, addr = sock.accept()
+        received = client.recv(10485760).decode()
+        filename, filesize = received.split('-')
+        filename = os.path.basename(filename)
+        filesize = int(filesize)
+        progress = tqdm(range(filesize), f"Progress | ",unit="B", unit_scale=True, unit_divisor=1024)
+        with open(filename, "wb") as f:
+            while True:
+                bytes_read = client.recv(10485760)
+                if not bytes_read:
+                    break
+                f.write(bytes_read)
+                progress.update(len(bytes_read))
+            print(f"Download complete {filename} {filesize} Bytes")
+            client.close()
+            sock.close()
+            sys.exit()
 
 
 def client_handle(client_socket,address):
@@ -163,15 +147,16 @@ def client_handle(client_socket,address):
         try:
             with client_socket as sock:
                 while True: 
-                    recv = sock.recv(10485760) 
+                    recv = sock.recv(4096) 
                     #print(recv.decode()) 
                     try:
-                        sock.sendall(exec(recv.decode()))
+                        sock.send(exec(recv.decode()))
                     except :
                         sock.send(b"\n\r")
         except:
             client_socket.close()
             print("connection closed from",addr)
+  
     elif args.listen:
         addr = address
         try:
@@ -180,7 +165,7 @@ def client_handle(client_socket,address):
                 recv = sock.recv(4096) 
                         #print(recv.decode()) 
                 try:
-                    sock.sendall(exec(recv.decode()))
+                    sock.send(exec(recv.decode()))
                 except :
                     sock.send(b"Error Occured")
         except:
@@ -190,6 +175,7 @@ def client_handle(client_socket,address):
             client_socket.close()
             print("connection closed from",addr)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Netcat Tool",formatter_class=
     argparse.RawDescriptionHelpFormatter,epilog=textwrap.dedent("""
@@ -198,15 +184,18 @@ if __name__ == "__main__":
     netcat.py -t  192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
     netcat.py -t  192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
     netcat.py -t  192.168.1.108 -p 5555  #  connect to server
+    netcat.py -t  192.168.1.108 -p 5555 -d  download file 
+    netcat.py -t  192.168.1.108 -p 5555 -u -f <file path> upload file file 
     echo 'ABC' |  ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135"""))
-    
-parser.add_argument("-s","--shell",action='store_true',help='command shell')
-parser.add_argument('-e', '--execute', help='execute specified command')
-parser.add_argument('-l', '--listen',action='store_true',help='listen')
-parser.add_argument('-p', '--port',type=int, help='specified port')
-parser.add_argument('-t', '--target',help='specified IP')
-parser.add_argument('-u', '--upload', help='upload file')
-
-args = parser.parse_args()
-
-main(args)
+        
+    parser.add_argument("-s","--shell",action='store_true',help='command shell')
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('-l', '--listen',action='store_true',help='listen')
+    parser.add_argument('-p', '--port',type=int, help='specified port')
+    parser.add_argument('-t', '--target',help='specified IP')
+    parser.add_argument('-u', '--upload',action='store_true', help='upload file')
+    parser.add_argument('-d', '--download',action='store_true', help='download file file')
+    parser.add_argument('-f', '--file',help='File path to upload')
+    args = parser.parse_args()
+    main(args)
+    """coded by Atul"""
