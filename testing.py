1097e92650817d4615fa86b86e9ccf1eb6f66c9b
Atul Sutar <78650734+Atul-Sutar@users.noreply.github.com>
2021-08-28 18:52:13 +0530

    small code changes
    moved if main to last
    and main moved inside if main

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
-import os
 from threading import Thread
+import os
 import socket
 import argparse
 import shlex
@@ -51,59 +51,8 @@ def exec(cmd):
             return out
         # return str(out)+str(err)
     return "Command Not Entered"
-    
-    
 
-# def not_in_use_base():
-#     ip = "127.0.0.1"
-#     port = 43
 
-#     def main():
-#         sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#         sock.bind((ip,port))
-#         sock.listen(5)
-#         print(f"listening on {ip}:{port}")
-#         try:
-#             while True:
-#                 client, addr = sock.accept()
-#                 print(addr,"connected")
-#                 handle = Thread(target=client_handle,args=(client,addr))
-#                 handle.start()
-#         except:
-#             sock.close()
-#             print("terminated")
-
-#     def client_handle(client_socket,address):
-#         addr = address
-#         try:
-#             with client_socket as sock:
-#                 #while True: 
-#                 recv = sock.recv(10240) 
-#                     #print(recv.decode()) 
-#                 try:
-#                     sock.send(exec(recv.decode()))
-#                 except :
-#                     sock.send(b"Error Occured")
-#         except:
-#             client_socket.close()
-#             print("connection closed from",addr)
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Netcat Tool",formatter_class=
-    argparse.RawDescriptionHelpFormatter,epilog=textwrap.dedent("""
-    Example:
-    netcat.py -t  192.168.1.108 -p 5555 -l -c # command shell
-    netcat.py -t  192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
-    netcat.py -t  192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
-    netcat.py -t  192.168.1.108 -p 5555  #  connect to server
-    echo 'ABC' |  ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135"""))
-    
-parser.add_argument("-s","--shell",action='store_true',help='command shell')
-parser.add_argument('-e', '--execute', help='execute specified command')
-parser.add_argument('-l', '--listen',action='store_true',help='listen')
-parser.add_argument('-p', '--port',type=int, help='specified port')
-parser.add_argument('-t', '--target',help='specified IP')
-parser.add_argument('-u', '--upload', help='upload file')
 
 args = parser.parse_args()
 
@@ -204,5 +153,21 @@ def client_handle(client_socket,address):
         finally:
             client_socket.close()
             print("connection closed from",addr)
-
-main(args)
+            
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Netcat Tool",formatter_class=
+    argparse.RawDescriptionHelpFormatter,epilog=textwrap.dedent("""
+    Example:
+    netcat.py -t  192.168.1.108 -p 5555 -l -c # command shell
+    netcat.py -t  192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
+    netcat.py -t  192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
+    netcat.py -t  192.168.1.108 -p 5555  #  connect to server
+    echo 'ABC' |  ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135"""))
+    
+    parser.add_argument("-s","--shell",action='store_true',help='command shell')
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('-l', '--listen',action='store_true',help='listen')
+    parser.add_argument('-p', '--port',type=int, help='specified port')
+    parser.add_argument('-t', '--target',help='specified IP')
+    parser.add_argument('-u', '--upload', help='upload file')
+    main(args)
