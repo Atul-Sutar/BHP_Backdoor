4177dcfd15c93a6f2257aea562cb07422cc4af55
Atul Sutar <78650734+Atul-Sutar@users.noreply.github.com>
2021-09-30 17:09:17 +0530

    Update BHP_Backdoor.py
    minor update -c to -s

--- BHP_Backdoor.py
+++ BHP_Backdoor.py
@@ -180,7 +180,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Netcat Tool",formatter_class=
     argparse.RawDescriptionHelpFormatter,epilog=textwrap.dedent("""
     Example:
-    netcat.py -t  192.168.1.108 -p 5555 -l -c # command shell
+    netcat.py -t  192.168.1.108 -p 5555 -l -s # command shell
     netcat.py -t  192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
     netcat.py -t  192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
     netcat.py -t  192.168.1.108 -p 5555  #  connect to server
